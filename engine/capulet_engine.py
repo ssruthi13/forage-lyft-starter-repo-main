97df639dd753eab9cb5b7e233577f1a6abbb236d
sai sruthi chalasani <saisruthi1999@gmail.com>
2023-09-05 13:17:46 -0400

    first commit

---
+++ engine/capulet_engine.py
@@ -0,0 +1,9 @@
+from engine.engine import Engine
+
+class CapuletEngine(Engine):
+    def __init__(self, current_mileage, last_service_mileage):
+        self.current_mileage = current_mileage
+        self.last_service_mileage = last_service_mileage
+
+    def need_service(self) -> bool:
+        return self.current_mileage - self.last_service_mileage >= 30000
